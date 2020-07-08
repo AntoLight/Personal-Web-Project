3983b654673734723f1db6e321543137e58c65c6
AntoLight <antonioglorioso1@hotmail.com>
2020-07-07 22:12:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NebulaTech
+ This is just a personal website that i use to practice my new knowledge on different languages. HTML5, CSS3, JavaScript, PHP, Mysql are some of the languages that i used an i have plans to use in this document. Any recommendation or help is welcome I started coding from this year from 0, planning to have a base for University.
---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    // variables-Nav-Header
+    var menu_i = $("#menu_i");
+    var menu_nav = $("#cabeza");
+
+    // Nav-Header
+
+    menu_i.on("click", function () {
+        menu_nav.slideToggle();
+    });
+
+    // Menu-Fijo
+    var windowsAltura = $(window).height();
+    var barraAltura = $(".barra-principal").innerHeight();
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > windowsAltura) {
+            $(".barra-principal").addClass("fixed");
+            $("body").css({ "margin-top": barraAltura + "px" });
+        } else {
+            $(".barra-principal").removeClass("fixed");
+            $("body").css({ "margin-top": "0px" });
+        }
+    });
+
+    // Programacion-Leng.
+
+    $(".programas_contenido div").hide();
+    $(".programas_enlaces a:first-child").addClass("activo");
+    $("#java").show();
+
+    $(".programas_enlaces a").on("click", function () {
+        $(".programas_enlaces a").removeClass("activo");
+        var contenidoProgramas = $(this).attr("href");
+        $(".programas_contenido div").hide();
+        $(this).addClass("activo");
+        $(contenidoProgramas).slideDown();
+        return false;
+    });
+});
